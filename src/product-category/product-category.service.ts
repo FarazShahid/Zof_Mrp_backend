0e7cd9fbc01c3dc6dd2bc78c30cab63cf13743e0
hassaan-genx <hassaan@genxintegratedsystems.com>
2025-01-29 20:24:15 +0500

    Adding Errors in Category CRUD

--- src/product-category/product-category.service.ts
+++ src/product-category/product-category.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ProductCategory } from './entities/product-category.entity';
@@ -11,6 +11,14 @@ export class ProductCategoryService {
   ) {}
 
   async create(data: Partial<ProductCategory>): Promise<ProductCategory> {
+    const existingCategory = await this.categoryRepository.findOne({
+      where: { type: data.type },
+    });
+
+    if (existingCategory) {
+      throw new BadRequestException(`Category with this type already exists.`);
+    }
+
     return await this.categoryRepository.save(data);
   }
 
@@ -19,15 +27,39 @@ export class ProductCategoryService {
   }
 
   async findOne(id: number): Promise<ProductCategory> {
-    return await this.categoryRepository.findOne({ where: { id } });
+    const category = await this.categoryRepository.findOne({ where: { id } });
+
+    if (!category) {
+      throw new NotFoundException(`Categor not found.`);
+    }
+
+    return category;
   }
 
   async update(id: number, data: Partial<ProductCategory>): Promise<ProductCategory> {
+    const category = await this.findOne(id);
+    if (data.type) {
+      const existingCategory = await this.categoryRepository.findOne({
+        where: { type: data.type },
+      });
+
+      if (existingCategory && existingCategory.id !== id) {
+        throw new BadRequestException(`Category with this type already exists.`);
+      }
+    }
     await this.categoryRepository.update(id, data);
     return this.findOne(id);
-  }
+}
 
-  async remove(id: number): Promise<void> {
-    await this.categoryRepository.delete(id);
-  }
+
+async remove(id: number): Promise<{ message: string }> {
+    const deleteResult = await this.categoryRepository.delete(id);
+
+    if (!deleteResult.affected) {
+        throw new NotFoundException(`Category not found.`);
+    }
+
+    return { message: `Category has been deleted.` };
+}
+
 }
