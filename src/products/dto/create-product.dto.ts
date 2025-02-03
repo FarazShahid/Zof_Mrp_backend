1e09060efb11ff84800674af135cd6b9c8fe974d
hassaan-genx <hassaan@genxintegratedsystems.com>
2025-02-03 16:20:39 +0500

    Product Main Crud Without Details and Colrs ETC

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,20 @@
-export class CreateProductDto {}
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty({ message: 'ProductCategoryId is required' })
+  ProductCategoryId: number;
+
+  @IsNotEmpty({ message: 'FabricTypeId is required' })
+  FabricTypeId: number;
+
+  @IsNotEmpty({ message: 'Name is required' })
+  Name: string;
+
+  @IsNotEmpty({ message: 'CreatedBy is required' })
+  CreatedBy: string;
+
+  @IsNotEmpty({ message: 'UpdatedBy is required' })
+  UpdatedBy: string;
+
+  Description?: string;
+}
