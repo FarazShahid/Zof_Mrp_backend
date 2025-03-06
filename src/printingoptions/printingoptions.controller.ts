aa8f2fa1850c048017ab2fcd7c2da8dd18243d6b
hassaan-genx <hassaan@genxintegratedsystems.com>
2025-03-06 13:03:35 +0500

    Printing Option Crud Compleataion

--- src/printingoptions/printingoptions.controller.ts
+++ src/printingoptions/printingoptions.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { PrintingoptionsService } from './printingoptions.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CreatePrintingOptionDto, UpdatePrintingOptionDto } from './dto/printing-option.dto';
 
 @Controller('printingoptions')
 @UseGuards(JwtAuthGuard)
@@ -10,5 +11,25 @@ export class PrintingoptionsController {
   @Get()
   async findAll() {
     return this.printingOptionsService.getAllPrintingOptions();
+  }
+
+  @Post()
+  create(@Body() createPrintingOptionDto: CreatePrintingOptionDto) {
+    return this.printingOptionsService.create(createPrintingOptionDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.printingOptionsService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updatePrintingOption: UpdatePrintingOptionDto) {
+    return this.printingOptionsService.update(+id, updatePrintingOption);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: number): Promise<void> {
+    return this.printingOptionsService.remove(id);
   }
 }
