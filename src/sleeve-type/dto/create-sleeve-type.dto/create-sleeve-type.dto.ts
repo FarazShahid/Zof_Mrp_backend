d60242eaae271567d99610a9f635a9636d18bc0c
hassaan-genx <hassaan@genxintegratedsystems.com>
2025-03-13 15:28:39 +0500

    Sleeve Type Crud According to New Code Structre

--- src/sleeve-type/dto/create-sleeve-type.dto/create-sleeve-type.dto.ts
+++ src/sleeve-type/dto/create-sleeve-type.dto/create-sleeve-type.dto.ts
@@ -4,10 +4,7 @@ export class CreateSleeveTypeDto {
   @IsNotEmpty({ message: 'Sleeve Type Name is required' })
   sleeveTypeName: string;
 
-  @IsInt({ message: 'ProductCategoryId must be a valid integer' })
-  @Min(1, { message: 'ProductCategoryId must be greater than 0' })
+  @IsInt({ message: 'Must be a valid integer' })
+  @Min(1, { message: 'Must be greater than 0' })
   productCategoryId: number;
-
-  @IsNotEmpty({ message: 'CreatedBy is required' })
-  createdBy: string;
 }
--- src/sleeve-type/sleeve-type.controller.ts
+++ src/sleeve-type/sleeve-type.controller.ts
@@ -1,36 +1,67 @@
-import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards, ParseIntPipe, HttpCode, HttpStatus, Query } from '@nestjs/common';
 import { SleeveTypeService } from './sleeve-type.service';
 import { CreateSleeveTypeDto } from "./dto/create-sleeve-type.dto/create-sleeve-type.dto";
 import { UpdateSleeveTypeDto } from './dto/update-sleeve-type.dto/update-sleeve-type.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/current-user.decorator';
+import { PaginationDto } from '../orders/dto/pagination.dto';
 
 @Controller('sleeve-type')
 @UseGuards(JwtAuthGuard)
 export class SleeveTypeController {
-  constructor(private readonly sleeveService: SleeveTypeService) {}
+  constructor(private readonly sleeveService: SleeveTypeService) { }
 
   @Post()
-  create(@Body() createDto: CreateSleeveTypeDto) {
-    return this.sleeveService.create(createDto);
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createDto: CreateSleeveTypeDto, @CurrentUser() currentUser: any) {
+    try {
+      return this.sleeveService.create(createDto, currentUser.email);
+    } catch (error) {
+      throw error;
+    }
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   findAll() {
-    return this.sleeveService.findAll();
+    try {
+      return this.sleeveService.findAll();
+    } catch (error) {
+      throw error;
+    }
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.sleeveService.findOne(id);
+    try {
+      return this.sleeveService.findOne(id);
+    } catch (error) {
+      throw error;
+    }
   }
 
   @Put(':id')
-  update(@Param('id', ParseIntPipe) id: number, @Body() updateDto: UpdateSleeveTypeDto) {
-    return this.sleeveService.update(id, updateDto);
+  @HttpCode(HttpStatus.OK)
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateDto: UpdateSleeveTypeDto,
+    @CurrentUser() currentUser: any
+  ) {
+    try {
+      return this.sleeveService.update(id, updateDto, currentUser.email);
+    } catch (error) {
+      throw error;
+    }
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id', ParseIntPipe) id: number) {
-    return this.sleeveService.remove(id);
+    try {
+      return this.sleeveService.remove(id);
+    } catch (error) {
+      throw error;
+    }
   }
 }
