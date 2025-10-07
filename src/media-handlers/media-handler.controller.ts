df4cfae892264f30db2a4e033eb014d3874fd8c7
hassaan-genx <hassaan@genxintegratedsystems.com>
2025-10-07 14:35:11 +0500

    Media Handler Change from Azure Blob to Amazon S3 Bucket

--- src/media-handlers/media-handler.controller.ts
+++ src/media-handlers/media-handler.controller.ts
@@ -6,10 +6,12 @@ import {
   UseInterceptors,
   Query,
   Get,
+  Param,
+  Res,
+  UseGuards,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { MediaHandlersService } from './media-handlers.service';
-import { ControllerAuthProtector } from 'src/common/decorators/controller-auth-protector';
 import {
   ApiBody,
   ApiConsumes,
@@ -20,16 +22,25 @@ import {
 } from '@nestjs/swagger';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 import { AuditInterceptor } from 'src/audit-logs/audit.interceptor';
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import { Response } from 'express';
+import { Readable } from 'stream';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
-@ControllerAuthProtector('Media Handler', 'media-handler')
+@ApiTags("Media Handler")
+@Controller("media-handler")
 @UseInterceptors(AuditInterceptor)
-@ApiTags('Media Handler')
 export class MediaHandlersController {
   constructor(private readonly mediaHandlersService: MediaHandlersService) {}
+  private s3Client = new S3Client({ region: process.env.AWS_REGION });
+  private bucketName = process.env.AWS_S3_BUCKET_NAME;
 
+  @UseGuards(JwtAuthGuard)
+@ApiBearerAuth('JWT-auth')
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  @ApiOperation({ summary: 'Upload a media file to Azure Blob Storage' })
+  @ApiOperation({ summary: 'Upload a media file to S3 (public URL)' })
   @ApiConsumes('multipart/form-data')
   @ApiQuery({
     name: 'referenceType',
@@ -71,6 +82,8 @@ export class MediaHandlersController {
     return url ?? null;
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT-auth')
   @Get('documents')
   @ApiOperation({ summary: 'Get documents by reference type and ID' })
   @ApiQuery({
@@ -91,5 +104,28 @@ export class MediaHandlersController {
       referenceId,
       typeId,
     );
+  }
+
+  @Get(':key')
+  async getFile(@Param('key') key: string, @Res() res: Response) {
+    try {
+      const command = new GetObjectCommand({
+        Bucket: this.bucketName,
+        Key: key,
+      });
+
+      const file = await this.s3Client.send(command);
+
+      if (file.ContentType) {
+        res.setHeader('Content-Type', file.ContentType);
+      } else {
+        res.setHeader('Content-Type', 'application/octet-stream');
+      }
+
+      const bodyStream = file.Body as Readable;
+      bodyStream.pipe(res);
+    } catch (err) {
+      res.status(404).send('File not found');
+    }
   }
 }
